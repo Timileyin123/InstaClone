3d04a5fae9a0463f7af536843f7dd79c12853e77
Timileyin Olafisoye <101237813+Timileyin123@users.noreply.github.com>
2023-05-22 21:14:32 +0100

    Create header and add image to the Post component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,7 +1,29 @@
 export default function Post({ id, username, userImg, img, caption }) {
   return (
-    <div>
-      <h1>{username}</h1>
+    <div className="bg-white my-7 border rounded-md">
+      <div className="flex item-center p-5">
+        <img
+          className="h-12 rounded-full object-cover border p-1 mr-3"
+          src={userImg}
+          alt={username}
+        />
+        <p className="font-bold flex-1">{username}</p>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="w-6 h-6"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M6.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM12.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM18.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
+          />
+        </svg>
+      </div>
+      <img src={img} className="object-cover w-full" alt="" />
     </div>
   );
 }
