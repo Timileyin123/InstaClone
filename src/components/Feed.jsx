7c4613e4ded96b8f706209835eaab932fa58e2be
Timileyin Olafisoye <101237813+Timileyin123@users.noreply.github.com>
2023-05-22 03:46:30 +0100

    Supply data using minifaker, map it within stories comp. and pass it as props to the story comp.

---
+++ src/components/Feed.jsx
@@ -0,0 +1,15 @@
+import Stories from "./Stories";
+
+export default function Feed() {
+  return (
+    <main>
+      <section>
+        <Stories />
+      </section>
+
+      <section>
+        <div></div>
+      </section>
+    </main>
+  );
+}
